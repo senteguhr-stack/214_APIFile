58f3548d4a67d41e82986490d11003bd096f75b1
senteguhr <senteguhr@gmail.com>
2026-08-10 10:42:45 +0700

    Update komik controller and API routes

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,7 @@ const penulisController = require('../controller/penulisController');
 const komikController = require('../controller/komikController');
 const genreController = require('../controller/genreController');
 const authMiddleware = require('../middleware/authMiddleware');
+const uploadMiddleware = require('../middleware/uploadsMiddelware');
 
 router.post("/register", penulisController.register);
 router.post("/login", penulisController.login);
@@ -14,7 +15,7 @@ router.put("/genre/:id", authMiddleware, genreController.update);
 router.delete("/genre/:id", authMiddleware, genreController.remove);
 
 router.get("/komik", authMiddleware, komikController.getAll);
-router.post("/komik", authMiddleware, komikController.create);
-router.put("/komik/:id", authMiddleware, komikController.update);
+router.post("/komik", authMiddleware, uploadMiddleware.single("gambar"), komikController.create);
+router.put("/komik/:id", authMiddleware, uploadMiddleware.single("gambar"), komikController.update);
 router.delete("/komik/:id", authMiddleware, komikController.remove);
 module.exports = router;
